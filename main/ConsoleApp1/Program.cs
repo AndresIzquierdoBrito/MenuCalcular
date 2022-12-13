de820e2c977ab5c606afd3f715eb898d98660cb5
Jose Javier Contreras Pestano <josejaviercontreraspestano@gmail.com>
2022-12-13 14:21:22 +0100

    Pues ya estaria

--- main/ConsoleApp1/Program.cs
+++ main/ConsoleApp1/Program.cs
@@ -2,6 +2,24 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Principio");
+        int num, num1, num2 = 0;
+        Console.WriteLine("Inserte un numero");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Inserte un numero");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Meteme un numero papi");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num == 1)
+            Console.WriteLine(num1 + num2);
+        
+        if (num == 2)
+            Console.WriteLine(num1 - num2);
+
+        if (num == 3)
+            Console.WriteLine(num1 * num2);
+
+        if (num == 4)
+            Console.WriteLine(num1 / num2);
+        Console.ReadKey();
     }
 }
