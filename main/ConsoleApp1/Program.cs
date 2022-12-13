81da63fe63422778287bea42d9f709513fadbd9a
Andres Izquierdo <andres.izbri@gmail.com>
2022-12-13 13:15:36 +0000

    Crear programa

--- main/ConsoleApp1/Program.cs
+++ main/ConsoleApp1/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using System.Runtime.InteropServices;
+using System.Threading.Channels;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Principio");
+        int userInput;
+        do
+        {
+            Console.WriteLine("Introduce un numero para elegir en el menu: ");
+            Console.Write("No: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+        }while(userInput == null);
+        
+        if(userInput == 1)
+            Console.WriteLine("Bien, uno.");
+        if(userInput == 2)
+            Console.WriteLine("Bien, dos.");
+        if(userInput == 3)
+            Console.WriteLine("Bien, tres.");
+        if(userInput == 4)
+            Console.WriteLine("Bien, cuatro.");
+        if(userInput == 5)
+            Console.WriteLine("Bien, cinco.");
     }
 }
